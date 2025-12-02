688ca5043b2e4c2ad04b9fe4a50be8ebcddfd7b3
Gavin Hensley <gavinhensley@protonmail.com>
2025-12-02 14:31:26 -0500

    Adjusted padding

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { ReportForm } from "@/components/report-form";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-12 flex flex-col items-center bg-zinc-900 justify-center">
+    <div className="min-h-screen p-2 flex flex-col items-center bg-zinc-900 justify-center">
       <ReportForm />
     </div>
   );
